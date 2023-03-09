317788996ca034122ce2973f4357777a868bf651
pinkal <pinkal@audertec.com>
2023-03-09 22:55:57 +0530

    email added

--- BojoTravels/BojoTravels/settings.py
+++ BojoTravels/BojoTravels/settings.py
@@ -105,6 +105,15 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+# Email settings
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'pinkalsanoria123@gmail.com'  # replace with your actual email
+EMAIL_HOST_PASSWORD = 'oisniuxbhrrrmpxw'  
+
+
 
 # Internationalization
 # https://docs.djangoproject.com/en/4.0/topics/i18n/
--- BojoTravels/contactapp/views.py
+++ BojoTravels/contactapp/views.py
@@ -1,5 +1,30 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.core.mail import send_mail
+from django.conf import settings
+from django.contrib import messages
+from .models import Contact
 
-# Create your views here.
 def contact(request):
-    return render(request,'contactapp/contact.html')
+    if request.method == 'POST':
+        name = request.POST.get('name')
+        phone = request.POST.get('phone')
+        email = request.POST.get('email')
+        message = request.POST.get('message')
+
+        contact = Contact(name=name, phone=phone, email=email, message=message)
+        contact.save()
+        
+        
+         # send email
+        subject = 'New message from your website'
+        message = f'Name: {name}\nPhone: {phone}\nEmail: {email}\nMessage: {message}'
+        from_email = settings.DEFAULT_FROM_EMAIL
+        recipient_list = [settings.DEFAULT_FROM_EMAIL]
+        send_mail(subject, message, from_email, recipient_list, fail_silently=False)
+        messages.success(request, 'Your message has been sent!')
+        return redirect('contact')
+
+    return render(request, 'contactapp/contact.html')
+
+# def contact(request):
+#     return render(request,'contactapp/contact.html')
