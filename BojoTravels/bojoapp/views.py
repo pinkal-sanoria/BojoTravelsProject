c9c71a6d93a9048a3283a792b986a16d561a7adb
pinkal <pinkal@audertec.com>
2023-03-10 19:56:43 +0530

    working in cart

--- BojoTravels/bojoapp/views.py
+++ BojoTravels/bojoapp/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
-
+from bookingapp.models import Package
 # Create your views here.
 def index(request):
-    return render(request,'bojoapp/index.html')
+    packages = Package.objects.all()
+    return render(request,'bojoapp/index.html',{'packages': packages})
+
